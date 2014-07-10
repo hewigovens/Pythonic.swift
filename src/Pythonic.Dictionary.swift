6ed67a287b4e61e859b1e1606ae663b1fb364c46
practicalswift <practicalswift@users.noreply.github.com>
2014-07-10 23:56:15 +0200

    Added dict.get(key).

--- src/Pythonic.Dictionary.swift
+++ src/Pythonic.Dictionary.swift
@@ -2,4 +2,8 @@ extension Dictionary : LogicValue {
     func getLogicValue() -> Bool {
         return len(self) != 0
     }
+
+    func get(key: KeyType) -> ValueType? {
+        return self[key]
+    }
 }
