50e493491f13746ed2105b25cd15c4b020820107
practicalswift <practicalswift@users.noreply.github.com>
2014-07-11 17:24:39 +0200

    Added list of functions and variables in Python's float.

--- src/Pythonic.Double.swift
+++ src/Pythonic.Double.swift
@@ -1,3 +1,12 @@
+// >>> filter(lambda s: not s.startswith("_"), dir(1.1))
+//   as_integer_ratio
+//   conjugate,
+//   fromhex,
+//   hex,
+//   imag
+//   is_integer
+//   real
+
 extension Double : LogicValue {
     func getLogicValue() -> Bool {
         return self != 0
--- src/Pythonic.Float.swift
+++ src/Pythonic.Float.swift
@@ -1,5 +1,14 @@
 import Darwin
 
+// >>> filter(lambda s: not s.startswith("_"), dir(1.1))
+//   as_integer_ratio
+//   conjugate,
+//   fromhex,
+//   hex,
+//   imag
+//   is_integer
+//   real
+
 extension Float : LogicValue {
     func getLogicValue() -> Bool {
         return self != 0
