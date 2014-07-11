dd85fc7d173eff2dc1d4eafad6c55d518d20e236
practicalswift <practicalswift@users.noreply.github.com>
2014-07-11 13:45:25 +0200

    Added dict.clear()

--- src/Pythonic.Dictionary.swift
+++ src/Pythonic.Dictionary.swift
@@ -3,6 +3,10 @@ extension Dictionary : LogicValue {
         return len(self) != 0
     }
 
+    mutating func clear() {
+        self.removeAll()
+    }
+
     func get(key: KeyType) -> ValueType? {
         return self[key]
     }
