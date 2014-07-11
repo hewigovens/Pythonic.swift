0ffd344274f4edb32a0dd574ffe0a598f8c99dd2
practicalswift <practicalswift@users.noreply.github.com>
2014-07-11 18:41:21 +0200

    Added list of functions and variables in Python's datetime.

--- src/Pythonic.NSDate.swift
+++ src/Pythonic.NSDate.swift
@@ -1,3 +1,41 @@
+// >>> filter(lambda s: not s.startswith("_"), dir(datetime.datetime))
+//   astimezone
+//   combine
+//   ctime
+//   date
+//   day
+//   dst
+//   fromordinal
+//   fromtimestamp
+//   hour
+//   isocalendar
+//   isoformat
+//   isoweekday
+//   max
+//   microsecond
+//   min
+//   minute
+//   month
+//   now
+//   replace
+//   resolution
+//   second
+//   strftime
+//   strptime
+//   time
+//   timetuple
+//   timetz
+//   today
+//   toordinal
+//   tzinfo
+//   tzname
+//   utcfromtimestamp
+//   utcnow
+//   utcoffset
+//   utctimetuple
+//   weekday
+//   year
+
 extension NSDate {
     class func utcnow() -> NSDate {
         return NSDate()
