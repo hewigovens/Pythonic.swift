ab9c7cee5e04f3d7c8e8ac24d4b3405f532321d0
practicalswift <practicalswift@users.noreply.github.com>
2014-07-11 17:25:05 +0200

    Added list of functions and variables in Python's int.

--- src/Pythonic.Int.swift
+++ src/Pythonic.Int.swift
@@ -1,3 +1,11 @@
+// >>> filter(lambda s: not s.startswith("_"), dir(1))
+//   bit_length
+//   conjugate
+//   denominator
+//   imag
+//   numerator
+//   real
+
 extension Int : LogicValue {
     func getLogicValue() -> Bool {
         return self != 0
