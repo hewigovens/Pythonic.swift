bb0268d4c201022327cba4eca58f77bfbc28e752
practicalswift <practicalswift@users.noreply.github.com>
2014-07-11 17:22:31 +0200

    Added list of functions and variables in Python's {}.

--- src/Pythonic.Dictionary.swift
+++ src/Pythonic.Dictionary.swift
@@ -1,3 +1,23 @@
+// >>> filter(lambda s: not s.startswith("_"), dir({}))
+//   clear
+//   copy
+//   fromkeys
+//   get
+//   has_key
+//   items
+//   iteritems
+//   iterkeys
+//   itervalues
+//   keys
+//   pop
+//   popitem
+//   setdefault
+//   update
+//   values
+//   viewitems
+//   viewkeys
+//   viewvalues
+
 extension Dictionary : LogicValue {
     func getLogicValue() -> Bool {
         return len(self) != 0
