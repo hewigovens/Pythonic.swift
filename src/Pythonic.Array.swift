9dbf8235f22a15c93f53aa60616178014a9930cb
practicalswift <practicalswift@users.noreply.github.com>
2014-07-11 20:07:06 +0200

    Added status for all functions.

--- src/Pythonic.Array.swift
+++ src/Pythonic.Array.swift
@@ -1,23 +1,18 @@
 // >>> filter(lambda s: not s.startswith("_"), dir([]))
-//   append
-//   count
-//   extend
-//   index
-//   insert
-//   pop
-//   remove
-//   reverse
-//   sort
+//   append: Already in Swift.
+//   count: Added.
+//   extend: Already in Swift.
+//   index: Added.
+//   insert: Failed to add. (TODO)
+//   pop: Added, but due to a compiler bug it must be defined in test file for tests to pass.
+//   remove: Added.
+//   reverse: Added as "reverseInPlace" to avoid name collission with built-in Array.reverse.
+//   sort: Failed to add. Name collission with built-in Array.sort. (TODO)
 
 extension Array : LogicValue {
     func getLogicValue() -> Bool {
         return len(self) != 0
     }
-
-    // TODO:
-    // arr.extend()
-    // arr.pop()
-    // arr.sort()
 
     mutating func clear() {
         self.removeAll()
