50abb155004820c758a2afa7bbb2307cdcc200cb
practicalswift <practicalswift@users.noreply.github.com>
2014-07-11 19:03:44 +0200

    Added list of functions and variables in Python's time.

--- src/Pythonic.time.swift
+++ src/Pythonic.time.swift
@@ -1,3 +1,22 @@
+// >>> filter(lambda s: not s.startswith("_"), dir(time))
+//   accept2dyear
+//   altzone
+//   asctime
+//   clock
+//   ctime
+//   daylight
+//   gmtime
+//   localtime
+//   mktime
+//   sleep
+//   strftime
+//   strptime
+//   struct_time
+//   time
+//   timezone
+//   tzname
+//   tzset
+
 class time {
     class func time() -> Double {
         let now = NSDate()
