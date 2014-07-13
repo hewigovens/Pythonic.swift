e4621ba4f359363face481041836fa0d888af18c
practicalswift <practicalswift@users.noreply.github.com>
2014-07-14 00:18:16 +0200

    Merged Pythonic.Float.swift into Pythonic.Double.swift.

--- src/Pythonic.Double.swift
+++ src/Pythonic.Double.swift
@@ -1,3 +1,5 @@
+// Python docs: https://docs.python.org/2/library/stdtypes.html
+//
 // >>> filter(lambda s: not s.startswith("_"), dir(1.1))
 //   as_integer_ratio
 //   conjugate
@@ -7,6 +9,8 @@
 //   is_integer: Implemented.
 //   real
 
+import Darwin
+
 extension Double : LogicValue {
     func getLogicValue() -> Bool {
         return self != 0
@@ -14,6 +18,20 @@ extension Double : LogicValue {
 
     func isInteger() -> Bool {
         return floor(self) == self
+    }
+
+    func is_integer() -> Bool {
+        return self.isInteger()
+    }
+}
+
+extension Float : LogicValue {
+    func getLogicValue() -> Bool {
+        return self != 0
+    }
+
+    func isInteger() -> Bool {
+        return Darwin.floor(self) == self
     }
 
     func is_integer() -> Bool {
