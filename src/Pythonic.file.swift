930843875c9e5af587b5eb417615a7a2515115e2
Kare Morstol <kare@nottoobadsoftware.com>
2014-07-19 22:55:39 +0200

    Moved the sequence-related function to its own extension.

--- src/Pythonic.file.swift
+++ src/Pythonic.file.swift
@@ -38,23 +38,10 @@ import Foundation
 
 typealias file = NSFileHandle
 
-extension NSFileHandle : Sequence {
+extension NSFileHandle {
     func read() -> String {
         let data: NSData = self.readDataToEndOfFile()
         return NSString(data: data, encoding: NSUTF8StringEncoding) as String
-    }
-
-    // TODO: Make this function read line-by-line instead of reading entire file at once.
-    func generate() -> GeneratorOf<String> {
-        var i = 0
-        var lines = self.readLines().map { $0 + "\n" }
-        return GeneratorOf<String> {
-            if i >= len(lines) {
-                return .None
-            } else {
-                return lines[i++]
-            }
-        }
     }
 
     func readLines() -> [String] {
@@ -68,4 +55,20 @@ extension NSFileHandle : Sequence {
     func close() {
         self.closeFile()
     }
+}
+
+extension NSFileHandle : Sequence {
+    
+    // TODO: Make this function read line-by-line instead of reading entire file at once.
+    func generate() -> GeneratorOf<String> {
+        var i = 0
+        var lines = self.readLines().map { $0 + "\n" }
+        return GeneratorOf<String> {
+            if i >= len(lines) {
+                return .None
+            } else {
+                return lines[i++]
+            }
+        }
+    }    
 }
