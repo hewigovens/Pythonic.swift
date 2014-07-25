8465535b63e8f8d86f88b1bc33d3509388a617d1
practicalswift <practicalswift@users.noreply.github.com>
2014-07-26 01:43:31 +0200

    Now running source checker on "make test".

--- src/Pythonic-linter.swift
+++ src/Pythonic-linter.swift
@@ -2,8 +2,8 @@
 
 import Pythonic
 
-if len(sys.argv) != 2 {
-    println("Usage: Pythonic-linter.swift <swift-file>")
+if len(sys.argv) <= 1 {
+    println("Usage: Pythonic-linter.swift <file name pattern such as *.swift>")
     sys.exit(0)
 }
 
@@ -16,22 +16,32 @@ func countLeadingSpaces(s: String) -> Int {
     return 0
 }
 
+func lintFile(fileName: String) -> Bool {
+    var passed = true
+    for (lineNumber, originalLine) in enumerate(open(fileName)) {
+        var numberOfLeadingSpaces = countLeadingSpaces(originalLine)
+        var lineWithoutLeadingSpaces = originalLine[numberOfLeadingSpaces..<len(originalLine)]
+        if re.search("\t", originalLine) {
+            println("ERROR – Line #\(lineNumber + 1) of \(fileName) contains an raw/unquoted tab (\t):")
+            println(originalLine.replace("\t", "\\t") + "\\n")
+            passed = false
+        } else if numberOfLeadingSpaces % 4 != 0 {
+            println("ERROR – Line #\(lineNumber + 1) of \(fileName) has indentation of \(numberOfLeadingSpaces) spaces which is not a multiple of four (4):")
+            println(originalLine + "\\n")
+            passed = false
+        } else if lineWithoutLeadingSpaces != lineWithoutLeadingSpaces.strip() {
+            println("ERROR – Line #\(lineNumber + 1) of \(fileName) contains trailing whitespace:")
+            println(originalLine + "\\n")
+            passed = false
+        }
+    }
+    return passed
+}
+
 var hasErrors = false
-var fileName = sys.argv[1]
-for (lineNumber, originalLine) in enumerate(open(fileName)) {
-    var numberOfLeadingSpaces = countLeadingSpaces(originalLine)
-    var lineWithoutLeadingSpaces = originalLine[numberOfLeadingSpaces..<len(originalLine)]
-    if re.search("\t", originalLine) {
-        println("ERROR – Line #\(lineNumber + 1) of \(fileName) contains an raw/unquoted tab (\t):")
-        println(originalLine.replace("\t", "\\t") + "\\n")
-        hasErrors = true
-    } else if numberOfLeadingSpaces % 4 != 0 {
-        println("ERROR – Line #\(lineNumber + 1) of \(fileName) has indentation of \(numberOfLeadingSpaces) spaces which is not a multiple of four (4):")
-        println(originalLine + "\\n")
-        hasErrors = true
-    } else if lineWithoutLeadingSpaces != lineWithoutLeadingSpaces.strip() {
-        println("ERROR – Line #\(lineNumber + 1) of \(fileName) contains trailing whitespace:")
-        println(originalLine + "\\n")
+var fileNames = sys.argv[1..<len(sys.argv)]
+for fileName in fileNames {
+    if !lintFile(fileName) {
         hasErrors = true
     }
 }
