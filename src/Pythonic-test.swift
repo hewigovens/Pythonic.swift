d95e4a0b32970f6d4ab8bba1cd010580a38737b4
practicalswift <practicalswift@users.noreply.github.com>
2014-07-29 02:06:34 +0200

    Added and re-ordered test cases.

--- src/Pythonic-test.swift
+++ src/Pythonic-test.swift
@@ -3,37 +3,44 @@
 import Pythonic
 
 // ** (power operator)
-assert(2**2 == 4)
-assert(2.0**2.0 == 4)
 assert(1 + 2**2 + 2 == 7)
 assert(1 + 2.0**2.0 + 2 == 7)
 assert(1**1 + 2.0**2.0 + 2 == 7)
+assert(2 * 2 ** 2 * 3 ** 2 * 3 ** 3 == 1944)
+assert(2**2 == 4)
+assert(2.0**2.0 == 4)
 
 // abs
+assert(abs(-0.1) == 0.1)
 assert(abs(-1) == 1)
 
 // all
-assert(all(["foo", "bar", "zonk"]))
-assert(all([true, true, true]))
 assert(!all(["", "bar", "zonk"]))
 assert(!all([false, false, false]))
 assert(!all([false, false, true]))
+assert(all(["foo", "bar", "zonk"]))
+assert(all([0.0001, 0.0001]))
+assert(all([true, true, true]))
 
 // any
-assert(any(["", "foo", "bar", "zonk"]))
-assert(any([false, false, true]))
+assert(!any([0, 0]))
 assert(!any([false, false, false]))
+assert(any(["", "foo", "bar", "zonk"]))
+assert(any([0.1, 0]))
+assert(any([false, false, true]))
 
 // bool
 assert(!bool(""))
-assert(bool("foo"))
 assert(!bool(0))
+assert(bool("foo"))
 assert(bool(1))
 assert(bool([1]))
+// assert(!bool(set([]))) // error: could not find an overload for 'assert' that accepts the supplied arguments
 
 // chr
 assert(chr(97) == "a")
 assert(chr(ord("b")) == "b")
+assert(ord(chr(255)) == 255)
 
 // cmp
 assert(cmp("bar", "bar") == 0)
@@ -42,22 +49,26 @@ assert(cmp("foo", "bar") == 1)
 assert(cmp(0, 0) == 0)
 assert(cmp(0, 1) == -1)
 assert(cmp(1, 0) == 1)
+// assert(cmp([1, 2, 3, 100], [2, 3, 4, 5]) == -1) – assertion fails
 
 // double.is_integer/isInteger
-assert(1.0.is_integer())
+assert(!0.000000000001.is_integer())
 assert(!1.1.is_integer())
+assert(1.0.is_integer())
 
 // file
 // TODO: Missing test.
 
 // float
 assert(!float(0.0))
+assert(float(0 + 0.0001))
 assert(float(0.00000001))
 assert(float(1.0))
 
 // float.is_integer
-assert(float(1.0).is_integer())
 assert(!float(1.1).is_integer())
+assert(float(1.0).is_integer())
+assert(float(100).is_integer())
 
 // hex
 assert(hex(0) == "0x0")
@@ -65,24 +76,29 @@ assert(hex(1) == "0x1")
 assert(hex(10) == "0xa")
 assert(hex(100) == "0x64")
 assert(hex(1000) == "0x3e8")
+assert(hex(10000000) == "0x989680")
 
 // int
 assert(int(1.1) == 1)
+assert(int(9.9) == 9)
 
 // len
 assert(len("") == 0)
+assert(len("\t") == 1)
 assert(len("foo") == 3)
 assert(len(["foo", "bar", "baz"]) == 3)
 assert(len(["foo", "bar"]) == 2)
 assert(len(["foo"]) == 1)
 
 // list
+assert(list([1, 2, 3]) == [1, 2, 3])
 assert(list([1, 2, 3]).count(1) == 1)
 
 // list.count
 assert([1, 2, 2, 3, 3, 3].count(1) == 1)
 assert([1, 2, 2, 3, 3, 3].count(2) == 2)
 assert([1, 2, 2, 3, 3, 3].count(3) == 3)
+assert([1, 2, 3].count(4) == 0)
 
 // list.index
 assert(["foo", "bar", "zonk"].index("zonk") == 2)
@@ -92,27 +108,41 @@ assert(list(["a", "b", "c"]).index("b") == 1)
 // long
 assert(long(1.1) == 1)
 
-// math.sqrt
-assert(math.sqrt(9) == 3)
-assert(math.pow(2, 2) == 4)
+// math.acos
+assert(math.acos(-1) == math.pi)
 
-// factorial
+// math.asin
+assert(math.asin(1) == math.pi / 2)
+
+// math.atan
+assert(math.atan(1) == math.pi / 4)
+
+// math.cos
+assert(math.cos(math.pi) == -1)
+
+// math.degrees
+assert(math.degrees(math.pi) == 180)
+
+// math.factorial
 assert(math.factorial(0) == 1)
 assert(math.factorial(20) == 2432902008176640000)
 
-// degrees / radians
-assert(math.degrees(math.pi) == 180)
+// math.pow
+assert(math.pow(2, 2) == 4)
+
+// math.radians
 assert(math.radians(270) == math.pi * 1.5)
 
-// trig
+// math.sin
 assert(math.sin(math.pi / 2) == 1)
-assert(math.cos(math.pi) == -1)
+
+// math.sqrt
+assert(math.sqrt(9) == 3)
+
+// math.tan
 // Python returns 0.999... for tan(π / 4)
-assert(math.tan(math.pi / 4) > 0.9999999)
 assert(math.tan(math.pi / 4) <= 1)
-assert(math.asin(1) == math.pi / 2)
-assert(math.acos(-1) == math.pi)
-assert(math.atan(1) == math.pi / 4)
+assert(math.tan(math.pi / 4) > 0.9999999)
 
 // max
 assert(max(1, 2) == 2)
@@ -135,6 +165,7 @@ assert(oct(1) == "01")
 assert(oct(10) == "012")
 assert(oct(100) == "0144")
 assert(oct(1000) == "01750")
+// assert(oct(100000000000) == "01351035564000") – assertion fails
 
 // ord
 assert(ord("a") == 97)
@@ -144,8 +175,8 @@ assert(ord(chr(98)) == 98)
 assert(os.getcwd())
 
 // os.path.exists
-assert(os.path.exists("/tmp/"))
 assert(!os.path.exists("/tmp.foo/"))
+assert(os.path.exists("/tmp/"))
 
 // random.random
 assert(random.random() < 1)
@@ -157,46 +188,64 @@ assert(random.randint(0, 10) <= 10)
 assert(random.randrange(0, 10) <= 9)
 
 // range
-assert(range(10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-assert(range(1, 11) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 assert(range(0) == [])
-assert(range(1, 0) == [])
-assert(range(0, 5, -1) == [])
 assert(range(0, 10, 2) == [0, 2, 4, 6, 8])
+assert(range(0, 5, -1) == [])
 assert(range(0, 50, 7) == [0, 7, 14, 21, 28, 35, 42, 49])
+assert(range(1, 0) == [])
+assert(range(1, 11) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+assert(range(10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 // re.search
+assert(re.search("[\r\n]", "foo\rfoo").group(0) == "\r")
 assert(!re.search("^bar", "foobarzonk"))
 assert(!re.search("hello", "foobarzonk"))
+assert(re.search("\r\n", "foo\r\nfoo").group(0) == "\r\n")
 assert(re.search("^foo", "foobarzonk"))
+assert(re.search("^foo", "foobarzonk").group(0) == "foo")
 assert(re.search("^foo.*zonk$", "foobarzonk"))
 assert(re.search("foo", "foobarzonk"))
 assert(re.search("o", "foobarzonk"))
-assert(re.search("\r\n", "foo\r\nfoo").group(0) == "\r\n")
-assert(re.search("^foo", "foobarzonk").group(0) == "foo")
 
 // re.match
+assert(list(re.match("(Hello (foo) (bar) (zonk))", "Hello foo bar zonk").groups()) == ["Hello foo bar zonk", "foo", "bar", "zonk"])
+assert(re.match("(Hello (foo) (bar) (zonk))", "Hello foo bar zonk").group(0) == "Hello foo bar zonk")
+assert(re.match("(Hello (foo) (bar) (zonk))", "Hello foo bar zonk").group(1) == "Hello foo bar zonk")
+assert(re.match("(Hello (foo) (bar) (zonk))", "Hello foo bar zonk").group(2) == "foo")
+assert(re.match("(Hello (foo) (bar) (zonk))", "Hello foo bar zonk").group(3) == "bar")
+assert(re.match("(Hello (foo) (bar) (zonk))", "Hello foo bar zonk").group(4) == "zonk")
+assert(list(re.match("Hello[ \t]*(.*)world", "Hello     Python world").groups())[0] == "Python ")
+assert(re.match("Hello[ \t]*(.*)world", "Hello     Python world").group(0) == "Hello     Python world")
 assert(!re.match("o", "foobarzonk"))
 assert(re.match("^foo", "foobarzonk"))
 assert(re.match("foo", "foobarzonk"))
+// assert(re.match("/(.*)/(.*)/(.*)", "/var/tmp/foo").groups() == ("var", "tmp", "foo")) // error: '[String]' is not convertible to 'MirrorDisposition'
+assert(re.match("Hello[ \t]*(.*)world", "Hello     Python world").group(1) == "Python ")
+assert(re.search("foo", "barfoobar"))
+assert(!re.search("zoo", "barfoobar"))
+assert(list(re.search("/(.*)/(.*)/(.*)", "/var/tmp/foo").groups()) == ["var", "tmp", "foo"])
+assert(re.match("/(.*)/(.*)/(.*)", "/var/tmp/foo").groups()[0] == "var")
+assert(re.match("/(.*)/(.*)/(.*)", "/var/tmp/foo").groups()[1] == "tmp")
+assert(re.match("/(.*)/(.*)/(.*)", "/var/tmp/foo").groups()[2] == "foo")
+assert(len(re.search("foo", "barfoobar").groups()) == 0)
 
 // re.split
-assert(re.split("/", "foo/bar") == ["foo", "bar"])
 assert(re.split("/", "") == [""])
 assert(re.split("/", "/") == ["", ""])
 assert(re.split("/", "foo/") == ["foo", ""])
+assert(re.split("/", "foo/bar") == ["foo", "bar"])
 assert(re.split("/", "foo/bar/") == ["foo", "bar", ""])
 assert(re.split("/", "foo/bar/zonk") == ["foo", "bar", "zonk"])
 assert(re.split("/", "foo/bar/zonk/") == ["foo", "bar", "zonk", ""])
+assert(re.split("[0-9]", "e8f8z888ee88ch838h23fhh3h2ui388sh3") == ["e", "f", "z", "", "", "ee", "", "ch", "", "", "h", "", "fhh", "h", "ui", "", "", "sh", ""])
 assert(re.split("[0-9]", "foo/bar/zonk") == ["foo/bar/zonk"])
 assert(re.split("[0-9]", "foo/bar/zonk/") == ["foo/bar/zonk/"])
+assert(re.split("[XY]+", "aXYbXYc") == ["a", "b", "c"])
+assert(re.split("[\r\n]", "\r\n\t\t\r\n\r\t\n\r\r\n\n\t\t") == ["", "", "\t\t", "", "", "\t", "", "", "", "", "\t\t"])
+assert(re.split("[\r\n]+", "foo\naw\tef\roa\r\nwef") == ["foo", "aw\tef", "oa", "wef"])
 assert(re.split("[^a-z]", "e8f8z888ee88ch838h23fhh3h2ui388sh3") == ["e", "f", "z", "", "", "ee", "", "ch", "", "", "h", "", "fhh", "h", "ui", "", "", "sh", ""])
 assert(re.split("[a-z]", "e8f8z888ee88ch838h23fhh3h2ui388sh3") == ["", "8", "8", "888", "", "88", "", "838", "23", "", "", "3", "2", "", "388", "", "3"])
 assert(re.split("a-z", "e8f8z888ee88ch838h23fhh3h2ui388sh3") == ["e8f8z888ee88ch838h23fhh3h2ui388sh3"])
-assert(re.split("[0-9]", "e8f8z888ee88ch838h23fhh3h2ui388sh3") == ["e", "f", "z", "", "", "ee", "", "ch", "", "", "h", "", "fhh", "h", "ui", "", "", "sh", ""])
-assert(re.split("[XY]+", "aXYbXYc") == ["a", "b", "c"])
-assert(re.split("[\r\n]+", "foo\naw\tef\roa\r\nwef") == ["foo", "aw\tef", "oa", "wef"])
-assert(re.split("[\r\n]", "\r\n\t\t\r\n\r\t\n\r\r\n\n\t\t") == ["", "", "\t\t", "", "", "\t", "", "", "", "", "\t\t"])
 
 // re.sub
 assert(re.sub("^foo", "bar", "foofoo") == "barfoo")
@@ -206,94 +255,48 @@ assert(re.sub("^zfoo", "bar", "foofoo") == "foofoo")
 assert(round(1.1) == 1)
 
 // set
-assert(set([1, 2, 3]) - set([3, 4, 5]) == set([1, 2]))
+assert(!(set([1, 2, 3]) < set([1, 2, 3])))
+assert(!(set([4]) < set([1, 2, 3])))
+assert(set([1, 1, 1, 2, 2, 3, 3, 4]) == set([1, 2, 3, 4]))
 assert(set([1, 2, 3]) & set([3, 4, 5]) == set([3]))
+assert(set([1, 2, 3]) - set([3, 4, 5]) == set([1, 2]))
 assert(set([1, 2, 3]) | set([3, 4, 5]) == set([1, 2, 3, 4, 5]))
 assert(set([1, 2, 3]))
-assert(set([1, 2]))
-assert(set([1]))
-assert(!(set([4]) < set([1, 2, 3])))
-assert(!(set([1, 2, 3]) < set([1, 2, 3])))
 assert(set([1, 2]) < set([1, 2, 3]))
+assert(set([1, 2]))
 assert(set([1]) < set([1, 2, 3]))
-assert(set([1, 1, 1, 2, 2, 3, 3, 4]) == set([1, 2, 3, 4]))
+assert(set([1]))
 
 // set.isdisjoint
-assert(set([1, 2, 3]).isdisjoint(set([4, 8, 16])))
 assert(!set([1, 2, 3]).isdisjoint(set([3, 4, 5])))
+assert(set([1, 2, 3]).isdisjoint(set([4, 8, 16])))
 
 // str
 assert(str(123) == "123")
 
 // str (positive and negative indexing)
-assert("foo"[0] == "f")
-assert("foo"[len("foo")-1] == "o")
 assert("foo"[-1] == "o")
 assert("foo"[-2] == "o")
+assert("foo"[0] == "f")
+assert("foo"[len("foo")-1] == "o")
 
 // str * int (repeat string)
 assert("foo" * 3 == "foofoofoo")
 assert("foo" * 3 == "foofoofoo")
+assert(-1 * "foo" == "")
 assert(0 * "foo" == "")
 assert(1 * "foo" == "foo")
 assert(2 * "foo" * 2 == "foofoofoofoo")
 assert(2 * "foo" == "foofoo")
 
 // str % tuple
-assert("foo %d" % (123) == "foo 123")
-assert("foo %% bar %011d zonk %s" % (100, "foobar") == "foo % bar 00000000100 zonk foobar")
+assert("Hello %d! Number %s" % (1, "world") == "Hello 1! Number world")
+assert("Hello %d!" % (1) == "Hello 1!")
+assert("Hello %s! Number %d" % ("world", 1) == "Hello world! Number 1")
+assert("Hello %s!" % ("world") == "Hello world!")
 assert("With commit %d, this string building syntax is now %s!" % (197, "supported") == "With commit 197, this string building syntax is now supported!")
-
-// str.isalnum
-assert("foo1".isalnum())
-assert(!"foo ".isalnum())
-
-// str.isalpha
-assert("fooo".isalpha())
-assert(!"foo1".isalpha())
-
-// str.isdigit
-assert("123".isdigit())
-assert(!"foo1".isdigit())
-
-// str.isspace
-assert(" \t".isspace())
-assert(!" a\t".isspace())
-assert("   ".isspace())
-assert(!"  x ".isspace())
-
-// str.islower
-assert("foo".islower())
-assert(!"FOO".islower())
-
-// str.isupper
-assert("FOO".isupper())
-assert(!"foo".isupper())
-
-// str.istitle
-assert("Foo".istitle())
-assert(!"foo".istitle())
-assert("Foo Foo".istitle())
-assert(!"foo foo".istitle())
-
-// str.lower
-assert("FooBar".lower() == "foobar")
-
-// str.replace
-assert("fzzbar".replace("z", "o") == "foobar")
-
-// str.strip
-assert("  \n   foobar       \n   ".strip() == "foobar")
-assert(" foobar ".strip() == "foobar")
-assert("".strip() == "")
-assert("foobar".strip() == "foobar")
-
-// str.lstrip/rstrip/strip
-assert(" \n\t foobar \n\t ".lstrip() == "foobar \n\t ")
-assert(" \n\t foobar \n\t ".rstrip() == " \n\t foobar")
-assert(" \n\t foobar \n\t ".strip() == "foobar")
-
-// str.lstrip
+assert("foo %% bar %011d zonk %s" % (100, "foobar") == "foo % bar 00000000100 zonk foobar")
+assert("foo %d" % (123) == "foo 123")
 
 // str.capitalize
 assert("".capitalize() == "")
@@ -301,6 +304,12 @@ assert("f".capitalize() == "F")
 assert("fo".capitalize() == "Fo")
 assert("foo baR".capitalize() == "Foo bar")
 assert("foo".capitalize() == "Foo")
+
+// str.center
+assert("foo".center(5) == " foo ")
+assert("foo".center(6, "-") == "-foo--")
+assert("foobar".center(9, "-") == "--foobar-")
+assert("foobar".center(4) == "foobar")
 
 // str.count
 assert("foo".count("f") == 1)
@@ -310,8 +319,68 @@ assert("foo".count("b") == 0)
 // str.endswith
 assert("foobar".endswith("bar"))
 
+// str.isalnum
+assert(!"foo ".isalnum())
+assert("foo1".isalnum())
+
+// str.isalpha
+assert(!"foo1".isalpha())
+assert("fooo".isalpha())
+
+// str.isdigit
+assert(!"foo1".isdigit())
+assert("123".isdigit())
+
+// str.islower
+assert(!"FOO".islower())
+assert("foo".islower())
+
+// str.isspace
+assert(!"  x ".isspace())
+assert(!" a\t".isspace())
+assert("   ".isspace())
+assert(" \t".isspace())
+
+// str.istitle
+assert(!"foo foo".istitle())
+assert(!"foo".istitle())
+assert("Foo Foo".istitle())
+assert("Foo".istitle())
+
+// str.isupper
+assert(!"foo".isupper())
+assert("FOO".isupper())
+
 // str.join
 assert(":".join(["foo", "bar", "baz"]) == "foo:bar:baz")
+
+// str.ljust
+assert("foo".ljust(5) == "foo  ")
+assert("foo".ljust(10, "-") == "foo-------")
+assert("foobar".ljust(4) == "foobar")
+
+// str.lower
+assert("FooBar".lower() == "foobar")
+
+// str.lstrip
+assert(" \n\t foobar \n\t ".lstrip() == "foobar \n\t ")
+
+// str.partition
+assert("the first part\nthe second part".partition("\n") == ("the first part","\n","the second part"))
+assert("the first part".partition("\n") == ("the first part", "", ""))
+assert("the first part\n".partition("\n") == ("the first part","\n",""))
+assert("\nthe second part".partition("\n") == ("","\n","the second part"))
+
+// str.replace
+assert("fzzbar".replace("z", "o") == "foobar")
+
+// str.rjust
+assert("foo".rjust(5) == "  foo")
+assert("foo".rjust(10, "-") == "-------foo")
+assert("foobar".rjust(4) == "foobar")
+
+// str.rstrip
+assert(" \n\t foobar \n\t ".rstrip() == " \n\t foobar")
 
 // str.split
 assert("foo:bar:baz".split(":") == ["foo", "bar", "baz"])
@@ -325,6 +394,13 @@ assert("\nfoo\rfoo\nfoo\r\nfoo\n\rfoo\nfoo\n".splitlines() == ["", "foo", "foo",
 // str.startswith
 assert("foobar".startswith("foo"))
 
+// str.strip
+assert("  \n   foobar       \n   ".strip() == "foobar")
+assert(" foobar ".strip() == "foobar")
+assert("".strip() == "")
+assert("foobar".strip() == "foobar")
+assert(" \n\t foobar \n\t ".strip() == "foobar")
+
 // str.swapcase
 assert("foo".swapcase() == "FOO")
 assert("FooBar".swapcase() == "fOObAR")
@@ -335,28 +411,6 @@ assert("foo bar".title() == "Foo Bar")
 
 // str.upper
 assert("FooBar".upper() == "FOOBAR")
-
-// str.ljust
-assert("foo".ljust(5) == "foo  ")
-assert("foo".ljust(10, "-") == "foo-------")
-assert("foobar".ljust(4) == "foobar")
-
-// str.rjust
-assert("foo".rjust(5) == "  foo")
-assert("foo".rjust(10, "-") == "-------foo")
-assert("foobar".rjust(4) == "foobar")
-
-// str.center
-assert("foo".center(5) == " foo ")
-assert("foo".center(6, "-") == "-foo--")
-assert("foobar".center(9, "-") == "--foobar-")
-assert("foobar".center(4) == "foobar")
-
-// str.partition
-assert("the first part\nthe second part".partition("\n") == ("the first part","\n","the second part"))
-assert("the first part".partition("\n") == ("the first part", "", ""))
-assert("the first part\n".partition("\n") == ("the first part","\n",""))
-assert("\nthe second part".partition("\n") == ("","\n","the second part"))
 
 // sum
 assert(sum([1, 2, 3]) == 6)
